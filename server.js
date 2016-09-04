16f7ca132ad8fdfed5860b1d261d821903159e96
Ross Money <Ross Money>
2016-09-04 14:24:15 +0100

    changed mongodb settings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ bodyParser = require("body-parser"),
 mongoose = require('mongoose'),
 fs = require('fs');
 
-var mongoUri = 'mongodb://localhost/musicians';
+var mongoUri = 'mongodb://musician:musician@ds147905.mlab.com:47905/musicians';
 mongoose.connect(mongoUri);
 var db = mongoose.connection;
 db.on('error', function () {
@@ -16,5 +16,5 @@ app.use(bodyParser.json());
 require('./app/models/musician');
 require('./app/routes')(app);
 
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
 console.log('Listening on port 3001...');
